bb9f1f8e3ab4b7a2246d08a01cd7661678e12594
Qasim Nawaz <qasim.nawaz@justice.gov.uk>
2024-12-18 13:44:11 +0000

    Add tests

--- src/test/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImplTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.adoption.model.common.Element;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
@@ -23,6 +27,10 @@ class DataMigrationServiceImplTest {
     private DataMigrationServiceImpl dataMigrationService;
 
     CaseDetails caseDetails;
+    CaseDetails caseDetailsInDraftState;
+    CaseDetails caseDetailsInAwaitingPaymentState;
+    CaseDetails caseDetailsInSubmittedState;
+    CaseDetails caseDetailsInLaSubmittedState;
 
     @BeforeEach
     void setUp() {
@@ -36,6 +44,46 @@ class DataMigrationServiceImplTest {
         caseDetails = CaseDetails.builder()
             .data(Map.of("court", court))
             .build();
+
+        caseDetailsInDraftState = CaseDetails.builder()
+            .state("Draft")
+            .createdDate(LocalDateTime.of(2024, 1, 1, 0, 0))
+            .data(Map.of("court", court))
+            .build();
+
+        caseDetailsInAwaitingPaymentState = CaseDetails.builder()
+            .state("AwaitingPayment")
+            .createdDate(LocalDateTime.of(2024, 1, 1, 0, 0))
+            .data(Map.of(
+                "court", court,
+                "application", Map.of(
+                    "dateSubmitted", LocalDateTime.of(2024, 1, 15, 0, 0),
+                    "applicationPayments",
+                            List.of(Element.newElement(Map.of(
+                                "created", LocalDateTime.of(2024, 1, 16, 0, 0),
+                                "amount", 250
+                            )))
+                )
+            ))
+            .build();
+
+        caseDetailsInSubmittedState = CaseDetails.builder()
+            .state("Submitted")
+            .createdDate(LocalDateTime.of(2024, 1, 1, 0, 0))
+            .data(Map.of(
+                "court", court,
+                "application", Map.of(
+                    "dateSubmitted", LocalDateTime.of(2024, 1, 15, 0, 0)
+                )
+            ))
+            .build();
+
+        caseDetailsInLaSubmittedState = CaseDetails.builder()
+            .state("LaSubmitted")
+            .createdDate(LocalDateTime.of(2024, 1, 1, 0, 0))
+            .lastModified(LocalDateTime.of(2024, 2, 1, 0, 0))
+            .data(Map.of("court", court))
+            .build();
     }
 
     @Test
@@ -46,7 +94,7 @@ class DataMigrationServiceImplTest {
 
     @Test
     void shouldThrowExceptionWhenMigrationKeyIsNotSet() {
-        assertThatThrownBy(() -> dataMigrationService.migrate(Map.of(), null))
+        assertThatThrownBy(() -> dataMigrationService.migrate(caseDetails, null))
             .isInstanceOf(NullPointerException.class)
             .hasMessage("Migration ID must not be null");
     }
@@ -54,10 +102,20 @@ class DataMigrationServiceImplTest {
     @Test
     void shouldThrowExceptionWhenMigrationKeyIsInvalid() {
         Map<String, Object> data = new HashMap<>();
-        assertThatThrownBy(() -> dataMigrationService.migrate(data, INVALID_MIGRATION_ID))
+        assertThatThrownBy(() -> dataMigrationService.migrate(caseDetails, INVALID_MIGRATION_ID))
             .isInstanceOf(NoSuchElementException.class)
             .hasMessage("No migration mapped to " + INVALID_MIGRATION_ID);
         assertThat(data.get(MIGRATION_ID_KEY)).isNull();
     }
 
+    @Test
+    void shouldPopulateTtlOnCaseInDraftState() {
+        LocalDate expectedSystemTtl = LocalDate.of(2024, 1, 1).plusDays(90);
+        Map<String, Object> expectedTtl = new HashMap<>();
+        expectedTtl.put("OverrideTTL", null);
+        expectedTtl.put("Suspend", "NO");
+        expectedTtl.put("SystemTTL", expectedSystemTtl);
+
+        assertThat(dataMigrationService.triggerTtlMigration(caseDetailsInDraftState).equals(expectedTtl));
+    }
 }
